28368e79856af41b4ca6e761af2ad84dc175031d
Jose_P <jspascual1111@gmail.com>
2021-08-19 22:27:22 -0400

    Tying up loose ends

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -27,3 +27,43 @@ ORDER BY emp_no, to_date DESC;
 
 -- Check the table
 SELECT * FROM unique_titles;
+
+SELECT COUNT(*), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+SELECT * FROM retiring_titles;
+DROP TABLE retiring_titles;
+
+-- End of Deliverable 1, beginning of Deliverable 2
+-- Create new table for the mentorship program
+SELECT DISTINCT ON (employees.emp_no) dept_emp.emp_no,
+	employees.first_name,
+	employees.last_name,
+	employees.birth_date,
+	dept_emp.from_date,
+	dept_emp.to_date
+INTO tempor
+FROM employees
+INNER JOIN dept_emp
+ON employees.emp_no = dept_emp.emp_no
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND (to_date = '9999-01-01');
+SELECT * FROM tempor;
+
+SELECT DISTINCT ON (tempor.emp_no) titles.emp_no,
+	tempor.first_name,
+	tempor.last_name,
+	tempor.birth_date,
+	tempor.from_date,
+	tempor.to_date,
+	titles.title
+INTO mentorship_eligibility
+FROM tempor
+INNER JOIN titles
+ON tempor.emp_no = titles.emp_no;
+SELECT * FROM mentorship_eligibility;
+
+-- DROP TABLE tempor;
+-- DROP TABLE mentorship_eligibility;
